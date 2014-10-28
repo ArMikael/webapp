3f037f192d6408fda98166cd33ebeae77cda5366
Michael <armikael2@gmail.com>
2014-10-28 23:41:32 +0300

    Adding styles to Reports window and creating js evnlistener

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+var reportsBtn = document.querySelector(".reports-btn"); 
 
+reportsBtn.onclick = function() {
+	var reportsDiv = document.querySelector(".reports"); 
+	if ( reportsDiv.style.display === "none" ) {
+			reportsDiv.style.display = "block";
+	} else {
+		reportsDiv.style.display = "none";
+	}
+};
