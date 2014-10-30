c902aad14fd47e4daad99595218be72e1428c53e
Michael <armikael2@gmail.com>
2014-10-30 10:48:05 +0300

    Adding styles to reports window

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var reportsBtn = document.querySelector(".reports-btn"); 
 
 reportsBtn.onclick = function() {
-	var reportsDiv = document.querySelector(".reports"); 
-	if ( reportsDiv.style.display === "none" ) {
-			reportsDiv.style.display = "block";
+	var reports = document.querySelector(".reports"); 
+	if ( reports.style.display === "none" ) {
+			reports.style.display = "block";
 	} else {
-		reportsDiv.style.display = "none";
+		reports.style.display = "none";
 	}
 };
