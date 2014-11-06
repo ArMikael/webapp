99e48395c3b51eff55371880c8e6d88d56ccdcc6
Michael <armikael2@gmail.com>
2014-11-06 10:08:07 +0300

    Adding accessibility on submenus on focus event in JS

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 window.onload = function() {
 
 	/* UTILS API  Section */
-	UTILS.ajax('data/notification.txt', {
-	done: function(response) {
-			//console.log(response);
-			var text = document.createTextNode(response);
-			var paragraph = document.createElement("p");
-			var notification = document.querySelector('.notifications');
-			paragraph.appendChild(text);
-			notification.appendChild(paragraph);
-		},
+	// UTILS.ajax('data/notification.txt', {
+	// done: function(response) {
+	// 		//console.log(response);
+	// 		var text = document.createTextNode(response);
+	// 		var paragraph = document.createElement("p");
+	// 		var notification = document.querySelector('.notifications');
+	// 		paragraph.appendChild(text);
+	// 		notification.appendChild(paragraph);
+	// 	},
 
-	fail: function(err) {
-		document.querySelector('#xhr');
-		}
-	});
+	// fail: function(err) {
+	// 	document.querySelector('#xhr');
+	// 	}
+	// });
 
 
 	/* JS Tabs Section */
@@ -33,6 +33,45 @@ window.onload = function() {
 
 	for ( var i = 0; i < tab.length; i++ ) {
 		UTILS.addEvent(tab[i], 'click', switchTab);
+		UTILS.addEvent(tab[i], 'focus', switchTab);
+	}
+
+	// var tabIndexArr = document.querySelectorAll('[tabindex="0"]');
+	// console.log(tabIndexArr);
+	var menuItems = document.querySelectorAll('.action-list a');
+	var lastItem = document.querySelectorAll('.action-list li:last-child a');
+	// console.log(lastItem);
+	var menus = document.querySelectorAll('.action-list');
+
+	/* This function open categories submenus on focus and highlighting currently
+	 selected list items */
+	var showMenu = function(e) {
+		var target = e.target,
+		parent = target.parentNode,
+		activeParent;
+
+		activeItem = document.querySelector("[data-item]");
+		activeItem.removeAttribute('data-item');
+		target.setAttribute('data-item', 'active');
+
+		parent.parentNode.style.height = "160px";
+		parent.parentNode.style.zIndex = "3";
+
+	};
+
+	var closeMenu = function(e) {
+		var target = e.target,
+		parent = target.parentNode;
+		console.log(parent.parentNode);
+		parent.parentNode.style.height = "0";
+	}
+
+	for ( var i = 0; i < menuItems.length; i++ ) {
+		UTILS.addEvent(menuItems[i], 'focus', showMenu);
+	}
+
+	for ( var i = 0; i < lastItem.length; i++ ) {
+		UTILS.addEvent(lastItem[i], 'blur', closeMenu);
 	}
 
 };
