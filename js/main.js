b611ce9be55b7e15829f8b66fab3875ecb2d02ef
Michael <armikael2@gmail.com>
2014-11-14 23:31:42 +0300

    Starting dev of onchange function for loading iframes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 window.onload = function() {
 
 	/* UTILS API  Section */
-	// UTILS.ajax('data/notification.txt', {
-	// done: function(response) {
-	// 		//console.log(response);
-	// 		var text = document.createTextNode(response);
-	// 		var paragraph = document.createElement("p");
-	// 		var notification = UTILS.qs('.notifications');
-	// 		paragraph.appendChild(text);
-	// 		notification.appendChild(paragraph);
-	// 	},
+	UTILS.ajax('data/notification.txt', {
+	done: function(response) {
+			//console.log(response);
+			var text = document.createTextNode(response);
+			var paragraph = document.createElement("p");
+			var notification = UTILS.qs('.notifications');
+			paragraph.appendChild(text);
+			notification.appendChild(paragraph);
+		},
 
-	// fail: function(err) {
-	// 	document.querySelector('#xhr');
-	// 	}
-	// });
+	fail: function(err) {
+		document.querySelector('#xhr');
+		}
+	});
 
 	/**
 	 * Tabs Section
@@ -88,25 +88,34 @@ window.onload = function() {
 
 
 	// Check if the Reports window in current tab is displayed and show it if needed
-	var openReports = function(event) {
-			// Finding new selected item
-			selectedOpt = sitesDropDown.querySelector('option[selected="selected"]');
-
-			var iframe = UTILS.qs('#qs-iframe'),
-			reports;
+	var openReports = function(e) {
+		var reports;
 
 		// Checks if the Event trigger is "Save" or "Cancel" button
 		if (event.currentTarget.nodeName === 'BUTTON') {
 			reports = event.currentTarget.parentNode;
 			UTILS.toggle(reports, 'active-window');
-
-			iframe.setAttribute('src', selectedOpt.value);
-
+			loadFrame(e);
 		// If not, the trigger was "Reports" button
 		} else {
 			reports = event.currentTarget.parentNode.querySelector(".reports");
 			UTILS.toggle(reports, 'active-window');
 		}
+	};
+
+	var loadFrame = function (e) {
+
+		// Solves the issue when user choose another option from the sites dropdown list
+		if (e.type === 'change') {
+			console.log(e.currentTarget);
+		}
+
+		var iframe = UTILS.qs('#qs-iframe');
+		// Finding new selected item
+		selectedOpt = sitesDropDown.querySelector('option[selected="selected"]');
+		// Changing iframe src to the currently added site
+		iframe.setAttribute('src', selectedOpt.value);
+
 	};
 
 	// Function that checks what event triggered and if on keypress "Enter" was clicked
@@ -267,6 +276,14 @@ window.onload = function() {
 
 	for (var i = 0; i < saveBtns.length; i++) {
 		UTILS.addEvent(saveBtns[i], 'click', checkNewSite);
+	};
+
+	// Listener that checks if anothor site was choosed by user in dropdown list.
+	var selects = UTILS.qsa('select');
+	console.log(selects);
+	for (var i = 0; i < selects.length; i++) {
+		console.log('New listener OK');
+		UTILS.addEvent(selects[i], 'change', loadFrame);
 	};
 
 };
